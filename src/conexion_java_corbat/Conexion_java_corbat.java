1e3186f7698d3f6694585275a48123086e9d3e46
USUARIO <USUARIO@PC16>
2018-09-19 20:54:14 -0500

    incio de proyecto

---
+++ src/conexion_java_corbat/Conexion_java_corbat.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package conexion_java_corbat;
+
+/**
+ *
+ * @author USUARIO
+ */
+public class Conexion_java_corbat {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
